4840a526c22e5160ec9f5cc771f01221a882a86f
maante <75334974+maante@users.noreply.github.com>
2023-02-09 13:27:00 -0500

    Job post basic
    Basic Job posting needs a lot of rework

---
+++ horizon/src/pages/jobpost/index.js
@@ -0,0 +1,50 @@
+const JobPostForm = () => (
+    <div className="w-64 mx-auto mt-10">
+      <h1 className="text-3xl font-medium text-center">Job Post</h1>
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="jtitle">
+            Job Title
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="jtitle"
+            type="text"
+            placeholder="Enter job title"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="jchar">
+            Job Characteristics
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="jchar"
+            type="text"
+            placeholder="Enter job characteristics"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 font-medium mb-2" htmlFor="jdescription">
+            Job Description
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="jdescription"
+            rows="5"
+            placeholder="Enter job description"
+          />
+        </div>
+        <div className="flex items-center justify-center">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+  
+  export default JobPostForm
