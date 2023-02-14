6aa235dfb2747b7eae3af6c728f5dfc1b63e5b0e
KubicRoots27 <71275640+KubicRoots27@users.noreply.github.com>
2023-02-14 14:34:52 -0500

    Addition of the jobPost.js class with get, publish, push, and delete functions. Addition of the jobPost.js file to create new Schema and connect to DB.

---
+++ horizon/src/pages/jobpost/jobPost.js
@@ -0,0 +1,49 @@
+// pages/api/jobpost.js
+import mongoose from "mongoose";
+import connectDB from "../../database/conn";
+import JobPosting from "../../models/JobPosting";
+
+const handleJobPost = async (req, res) => {
+  try {
+    const { method } = req;
+    switch (method) {
+      case "GET":
+        // Fetch all job postings
+        const jobPostings = await JobPosting.find({});
+        return res.status(200).json({ jobPostings });
+
+      case "POST":
+        // Create a new job posting
+        const newJobPost = new JobPosting(req.body);
+        await newJobPost.save();
+        return res.status(201).json({ newJobPost });
+
+      case "PUT":
+        // Update an existing job posting
+        const updatedJobPost = await JobPosting.findByIdAndUpdate(
+          req.query.id,
+          req.body,
+          {
+            new: true,
+          }
+        );
+        return res.status(200).json({ updatedJobPost });
+
+      case "DELETE":
+        // Delete a job posting
+        const deletedJobPost = await JobPosting.findByIdAndDelete(
+          req.query.id
+        );
+        return res.status(200).json({ deletedJobPost });
+
+      default:
+        return res.status(405).end();
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).end();
+  }
+};
+
+export default handleJobPost;
+
