10a77718ff0147d7b374e02643df8bbabc37907b
KubicRoots27 <71275640+KubicRoots27@users.noreply.github.com>
2023-02-28 17:35:14 -0500

    Update jobPost.js

--- horizon/src/pages/jobpost/jobPost.js
+++ horizon/src/pages/jobpost/jobPost.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import connectDB from "../../../../database/conn";
 import JobPosting from "../../../../models/JobPosting";
 
+export default async function handler(req, res) {
+  connectDB("UsersData").catch((error) => res.json({ error: error.message }));
+
 const handleJobPost = async (req, res) => {
   try {
     const { method } = req;
@@ -10,13 +13,13 @@ const handleJobPost = async (req, res) => {
       case "GET":
         // Fetch all job postings
         const jobPostings = await JobPosting.find({});
-        return res.status(200).json({ jobPostings });
+        return res.status(200).json({ "No such post found."});
 
       case "POST":
         // Create a new job posting
         const newJobPost = new JobPosting(req.body);
         await newJobPost.save();
-        return res.status(201).json({ newJobPost });
+        return res.status(201).json({ "Cannot post this job." });
 
       case "PUT":
         // Update an existing job posting
@@ -27,14 +30,14 @@ const handleJobPost = async (req, res) => {
             new: true,
           }
         );
-        return res.status(200).json({ updatedJobPost });
+        return res.status(200).json({ "Cannot update post." });
 
       case "DELETE":
         // Delete a job posting
         const deletedJobPost = await JobPosting.findByIdAndDelete(
           req.query.id
         );
-        return res.status(200).json({ deletedJobPost });
+        return res.status(200).json({ "Cannot delete post." });
 
       default:
         return res.status(405).end();
