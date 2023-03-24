ed8baed58907b0c0d346d534dc52ea704fdc356b
maante <75334974+maante@users.noreply.github.com>
2023-03-24 17:44:56 -0400

    Update index.js

--- horizon/src/pages/login/index.js
+++ horizon/src/pages/login/index.js
@@ -8,7 +8,7 @@ const Login = () => {
   const router = useRouter();
   
   return (
-    <div className="flex justify-center bg-cyan-600 h-26 p-16">
+    <div className="flex justify-center bg-cyan-600 dark:bg-cyan-900 h-26 p-16">
       <div className="bg-white dark:bg-black shadow-md rounded-md p-16 pb-8">
         <h1 className="text-3xl dark:text-white font-bold text-center pb-6">Login</h1>
         <Formik
