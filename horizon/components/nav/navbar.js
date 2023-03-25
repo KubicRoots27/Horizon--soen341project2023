7a42fb26aa778b72b2f9c27cd6c0776fe51467f9
Ashx11 <92754282+Ashx11@users.noreply.github.com>
2023-03-24 23:46:32 -0400

    Fixed the navbar code, styled the navbar and logo

--- horizon/components/nav/navbar.js
+++ horizon/components/nav/navbar.js
@@ -2,45 +2,84 @@ import Link from "next/link";
 import Image from "next/image";
 import React, { useState } from "react";
 
-import NavItem from "./navitem";
+// import NavItem from "./navitem";
 
 const MENU_LIST = [
   { text: "Home", href: "/" },
   { text: "Profile", href: "/profile" },
   { text: "Job Postings", href: "/jobpostings" },
 ];
+const LOGO_SOURCE =
+  "/logo.jpg";
+
 const Navbar = () => {
   const [navActive, setNavActive] = useState(null);
   const [activeIdx, setActiveIdx] = useState(-1);
-
+  const handleItemClick = (idx) => {
+    setActiveIdx(idx);
+  };
   return (
     <header>
-      <nav className={`nav`}>
-        <Link href={"/"}>
-          <h1 className="logo">Logo</h1>
-        </Link>
-        <div
-          onClick={() => setNavActive(!navActive)}
-          className={`nav__menu-bar`}
-        >
-          <div></div>
-          <div></div>
-          <div></div>
+      <nav className={`nav`} style={{ display: "flex", alignItems: "center" }}>
+        <div className="nav__left">
+        <Image src={LOGO_SOURCE} alt="Logo" width={100} height={50} style={{height: "60px"}} />
+
         </div>
-        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
-          {MENU_LIST.map((menu, idx) => (
-            <div
-              onClick={() => {
-                setActiveIdx(idx);
-                setNavActive(false);
-              }}
-              key={menu.text}
-            >
-              <NavItem active={activeIdx === idx} {...menu} />
-            </div>
+        <ul className="nav__right" style={{ display: "flex", alignItems: "center", marginLeft: "auto" }}>
+          {MENU_LIST.map((item, idx) => (
+            <li className={`nav__item ${idx === activeIdx ? "nav__item--active" : ""}`} key={idx}>
+              <Link href={item.href}>
+                <div className="nav__link" onClick={() => handleItemClick(idx)}>
+                  {item.text}
+                </div>
+              </Link>
+            </li>
           ))}
-        </div>
+        </ul>
       </nav>
+
+      <style jsx>{`
+        .nav {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          background-color: #f1f1f1;
+          height: 80px;
+          padding: 0 1rem;
+          border-bottom: 1px solid #ddd;
+        }
+
+        .nav__left {
+          display: flex;
+          align-items: center;
+        }
+
+        .nav__right {
+          display: flex;
+          align-items: center;
+          list-style: none;
+          margin: 0;
+          padding: 0;
+          justify-content: flex-end;
+        }
+
+        .nav__link {
+          cursor: pointer;
+          padding: 0.5rem;
+        }
+        .nav__item {
+          margin-right: 20px;
+        }
+
+        .nav__item--active {
+          background-color: #ddd;
+        }
+        
+        .nav__right li {
+          margin-left: 1rem;
+        }
+        
+      `}</style>
     </header>
   );
 };
