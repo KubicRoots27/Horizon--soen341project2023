0a442090489ef084517ac1896e3ac61b77abde98
mattmazzone <c_struty@live.concordia.ca>
2023-04-08 15:13:45 -0400

    authentication test

--- horizon/cypress/e2e/authentication.cy.js
+++ horizon/cypress/e2e/authentication.cy.js
@@ -8,4 +8,17 @@ describe("Login", () => {
 
     cy.url().should("include", "/profile");
   });
+
+  it("Logs out successfully", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('[name="email"]').type("christina.strutynski@spaceconcordia.ca");
+    cy.get('[name="password"]').type("111111");
+    cy.get("form").submit();
+
+    cy.url().should("include", "/profile");
+    cy.contains("Sign Out").click();
+
+    cy.contains("You are not logged in!").should("be.visible");
+  });
 });
