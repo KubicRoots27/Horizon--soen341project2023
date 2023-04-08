f2f0e33896bd7fb5bb258f56da8e4baabd4108a7
mattmazzone <c_struty@live.concordia.ca>
2023-04-07 22:20:46 -0400

    run cypress CI tests

---
+++ horizon/cypress/e2e/authentication.cy.js
@@ -0,0 +1,11 @@
+describe("Login", () => {
+  it("Logs in successfully", () => {
+    cy.visit("http://localhost:3000/login");
+
+    cy.get('[name="email"]').type("christina.strutynski@spaceconcordia.ca");
+    cy.get('[name="password"]').type("111111");
+    cy.get("form").submit();
+
+    cy.url().should("include", "/profile");
+  });
+});
